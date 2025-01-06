2f4ba252b9cae1c79ca2606181576e6a738bb963
Мурат <kerimovmurat@yandex.ru>
2025-01-07 02:10:46 +0300

    homework

---
+++ task1/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Имя покупателя')
+    balance = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Баланс')
+    age = models.IntegerField(verbose_name='Возраст')
+
+    def __str__(self):
+        return self.name
+
+class Game(models.Model):
+    title = models.CharField(max_length=100, unique=True, verbose_name='Название игры')
+    cost = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена игры')
+    size = models.DecimalField(max_digits=10, decimal_places=3, verbose_name='Размер файлов игры')
+    description = models.TextField(verbose_name='Описание')
+    age_limited = models.BooleanField(default=False, verbose_name='Возрастное ограничение 18+')
+    buyer = models.ManyToManyField(Buyer, related_name='games', verbose_name='Покупатели')
+
+    def __str__(self):
+        return self.title
