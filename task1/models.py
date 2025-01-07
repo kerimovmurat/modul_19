29dd343887a04ccd0437d6cd3ef2b23273a9148f
Мурат <kerimovmurat@yandex.ru>
2025-01-07 23:50:57 +0300

    homework2

--- task1/models.py
+++ task1/models.py
@@ -3,20 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Buyer(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name='Имя покупателя')
-    balance = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Баланс')
-    age = models.IntegerField(verbose_name='Возраст')
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    age = models.IntegerField()
 
     def __str__(self):
         return self.name
 
 class Game(models.Model):
-    title = models.CharField(max_length=100, unique=True, verbose_name='Название игры')
-    cost = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена игры')
-    size = models.DecimalField(max_digits=10, decimal_places=3, verbose_name='Размер файлов игры')
-    description = models.TextField(verbose_name='Описание')
-    age_limited = models.BooleanField(default=False, verbose_name='Возрастное ограничение 18+')
-    buyer = models.ManyToManyField(Buyer, related_name='games', verbose_name='Покупатели')
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=10, decimal_places=3)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer)
 
     def __str__(self):
         return self.title
