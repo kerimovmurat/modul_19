6830ac5c8b51f5dd1d9f04026f25918691969007
Мурат <kerimovmurat@yandex.ru>
2025-01-09 13:06:20 +0300

    homework4

--- task1/admin.py
+++ task1/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Buyer, Game
 
 # Register your models here.
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_filter = ('balance', 'age')
+    list_display = ('name', 'balance', 'age')
+    search_fields = ('name',)
+    readonly_fields = ('balance',)
+    list_per_page = 30
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_filter = ('size', 'cost')
+    list_display = ('title', 'cost', 'size')
+    search_fields = ('title',)
+    list_per_page = 20
+
--- task1/models.py
+++ task1/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Buyer(models.Model):
-    name = models.CharField(max_length=100)
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
-    age = models.IntegerField()
+    name = models.CharField(max_length=100, verbose_name='Имя игрока')
+    balance = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Баланс игрока')
+    age = models.IntegerField(verbose_name='Возраст игрока')
 
     def __str__(self):
         return self.name
